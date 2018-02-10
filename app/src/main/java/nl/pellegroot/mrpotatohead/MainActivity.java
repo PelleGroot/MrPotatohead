859ea9f5cd90d8b213c999ec532fcb5f9d099b2c
PelleGroot <pellegroot@gmail.com>
2018-02-10 11:06:32 +0100

    finished the code, it works now

--- app/src/main/java/nl/pellegroot/mrpotatohead/MainActivity.java
+++ app/src/main/java/nl/pellegroot/mrpotatohead/MainActivity.java
@@ -3,6 +3,7 @@ package nl.pellegroot.mrpotatohead;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.util.Log;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 
@@ -16,8 +17,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        potato = findViewById(R.id.potato);
-        arms = findViewById(R.id.arms);
+        potato = (ImageView) findViewById(R.id.potato);
+        arms = (ImageView) findViewById(R.id.arms);
         ears = findViewById(R.id.ears);
         eyebrows = findViewById(R.id.eyebrows);
         eyes = findViewById(R.id.eyes);
@@ -28,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         nose = findViewById(R.id.nose);
         shoes = findViewById(R.id.shoes);
 
-        checkArms = findViewById(R.id.checkArms);
+        checkArms = (CheckBox) findViewById(R.id.checkArms);
         checkEars = findViewById(R.id.checkEars);
         checkEyebrows = findViewById(R.id.checkEyebrows);
         checkEyes = findViewById(R.id.checkEyes);
@@ -41,73 +42,77 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void checkClicked(View v){
+        Log.d("potato", "checkClicked:");
+    }
+
     public void checkArmsClicked (View view){
-        if (arms.isShown())
-            arms.setVisibility(View.INVISIBLE);
-        else
+        if (checkArms.isChecked())
             arms.setVisibility(View.VISIBLE);
+        else
+            arms.setVisibility(View.INVISIBLE);
     }
 
     public void checkEarsClicked (View view){
-        if (ears.isShown())
-            ears.setVisibility(View.INVISIBLE);
-        else
+        if (checkEars.isChecked())
             ears.setVisibility(View.VISIBLE);
+        else
+            ears.setVisibility(View.INVISIBLE);
     }
 
     public void checkEyebrowsClicked (View view){
-        if (eyebrows.isShown())
-            eyebrows.setVisibility(View.INVISIBLE);
-        else
+        if (checkEyebrows.isChecked())
             eyebrows.setVisibility(View.VISIBLE);
+        else
+            eyebrows.setVisibility(View.INVISIBLE);
     }
 
     public void checkEyesClicked (View view){
-        if (eyes.isShown())
-            eyes.setVisibility(View.INVISIBLE);
-        else
+        if (checkEyes.isChecked())
             eyes.setVisibility(View.VISIBLE);
+        else
+            eyes.setVisibility(View.INVISIBLE);
     }
 
     public void checkGlassesClicked (View view){
-        if (glasses.isShown())
-            glasses.setVisibility(View.INVISIBLE);
-        else
+        if (checkGlasses.isChecked())
             glasses.setVisibility(View.VISIBLE);
+        else
+            glasses.setVisibility(View.INVISIBLE);
     }
 
     public void checkHatClicked (View view){
-        if (hat.isShown())
-            hat.setVisibility(View.INVISIBLE);
-        else
+        if (checkHat.isChecked())
             hat.setVisibility(View.VISIBLE);
+        else
+            hat.setVisibility(View.INVISIBLE);
     }
 
     public void checkMouthClicked (View view){
-        if (mouth.isShown())
-            mouth.setVisibility(View.INVISIBLE);
-        else
+        if (checkMouth.isChecked())
             mouth.setVisibility(View.VISIBLE);
+        else
+            mouth.setVisibility(View.INVISIBLE);
     }
 
     public void checkMoustacheClicked (View view){
-        if (moustache.isShown())
-            moustache.setVisibility(View.INVISIBLE);
-        else
+        if (checkMoustache.isChecked())
             moustache.setVisibility(View.VISIBLE);
+        else
+            moustache.setVisibility(View.INVISIBLE);
     }
 
     public void checkNoseClicked (View view){
-        if (nose.isShown())
-            nose.setVisibility(View.INVISIBLE);
-        else
+        if (checkNose.isChecked())
             nose.setVisibility(View.VISIBLE);
+        else
+            nose.setVisibility(View.INVISIBLE);
     }
 
     public void checkShoesClicked (View view){
-        if (shoes.isShown())
-            shoes.setVisibility(View.INVISIBLE);
-        else
+        if (checkShoes.isChecked())
             shoes.setVisibility(View.VISIBLE);
+        else
+            shoes.setVisibility(View.INVISIBLE);
     }
 }
